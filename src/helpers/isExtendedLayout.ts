1dd4392ed08c8de22a0094124414e01b70179f81
Maxwell Morrissey <mmorrissey92@proton.me>
2025-05-23 23:56:19 -0400

    Write isExtendedLayout function

--- src/helpers/isExtendedLayout.ts
+++ src/helpers/isExtendedLayout.ts
@@ -1,3 +1,27 @@
-function isExtendedLayout() {
-    //
+export function isExtendedLayout(): Promise<boolean> {
+    const queries = {
+        kittyProgressiveEnhancements: "\x1b[?u",
+        deviceAttributes: "\x1b[>0c",
+    };
+
+    const regex = /\[\?(\d+)u/gm;
+
+    return new Promise((res) => {
+        process.stdout.write(queries.kittyProgressiveEnhancements);
+        process.stdout.write(queries.deviceAttributes);
+
+        const handleData = (buf: Buffer) => {
+            const utf = buf.toString("utf-8");
+            const match = regex.exec(utf);
+            if (match && !Number.isNaN(match[1]) && Number(match[1]) !== 0) {
+                res(true);
+            } else {
+                res(false);
+            }
+
+            process.stdin.off("data", handleData);
+        };
+
+        process.stdin.on("data", handleData);
+    });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { setMouse } from "./setMouse.js";
 import { createRegister } from "./createRegister.js";
 import type { Key } from "./types.js";
+import { isExtendedLayout } from "./helpers/isExtendedLayout.js";
 
 process.stdin.setRawMode(true);
 setMouse(true, { stream: process.stdin, mode: 3 });
@@ -20,30 +21,32 @@ function dispatchEvent() {
 }
 
 // process.stdout.write("\x1b[>1u");
-process.stdout.write("\x1b[>8u");
+// process.stdout.write("\x1b[>8u");
 process.on("exit", () => process.stdout.write("\x1b[<u"));
 
-process.stdin.on("data", (buf) => {
-    console.clear();
-    parseBuffer(buf);
-
-    console.log("raw bytes:", data.raw.buffer.toString());
-    console.log("raw utf:", data.raw.utf);
-    console.log("key:", register.key.values());
-    console.log("input:", register.input.values());
-    console.log("mouse:", data.mouse ?? "-----");
-
-    console.log("-".repeat(process.stdout.columns));
-    console.log("data:", data);
-
-    if (register.key.only("ctrl") && register.input.has("c")) {
-        process.exit();
-    }
-
-    if (register.input.has("qq")) process.exit();
-
-    dispatchEvent();
-});
-
-console.clear();
-console.log("Type something or move the mouse...");
+isExtendedLayout().then(console.log);
+//
+// process.stdin.on("data", (buf) => {
+//     console.clear();
+//     parseBuffer(buf);
+//
+//     console.log("raw bytes:", data.raw.buffer.toString());
+//     console.log("raw utf:", data.raw.utf);
+//     console.log("key:", register.key.values());
+//     console.log("input:", register.input.values());
+//     console.log("mouse:", data.mouse ?? "-----");
+//
+//     console.log("-".repeat(process.stdout.columns));
+//     console.log("data:", data);
+//
+//     if (register.key.only("ctrl") && register.input.has("c")) {
+//         process.exit();
+//     }
+//
+//     if (register.input.has("qq")) process.exit();
+//
+//     dispatchEvent();
+// });
+//
+// console.clear();
+// console.log("Type something or move the mouse...");
