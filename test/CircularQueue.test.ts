80748cd14a5b6b4efc4765727968a1a57daf6a95
Maxwell Morrissey <mmorrissey92@proton.me>
2025-06-02 18:35:51 -0400

    Rewrite CircularQueue test suite to use test.each

--- test/CircularQueue.test.ts
+++ test/CircularQueue.test.ts
@@ -1,38 +1,64 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { CircularQueue } from "../src/stateful/CircularQueue.js";
 
 describe("CircularQueue", () => {
-    test("public: enqueue, forEach, size, tail", () => {
-        const q = new CircularQueue<number>(5);
+    const q = new CircularQueue<number>(5);
 
-        q.enqueue(0);
-        q.enqueue(1);
-        q.enqueue(2);
-        q.enqueue(3);
-        q.enqueue(4);
-        q.enqueue(5);
-        q.enqueue(6);
-        q.enqueue(7);
-        q.enqueue(8);
-        q.enqueue(9);
+    // prettier-ignore
+    test.each([
+        [0],
+        [1],
+        [2],
+        [3],
+        [4],
+        [5],
+        [6],
+        [7],
+        [8],
+        [9],
+    ])("Enqueue and tail %i", (num) => {
+        q.enqueue(num);
+        expect(q.tail()).toBe(num);
+    })
 
+    test("Size does not exceed size provided by constructor", () => {
         expect(q.size).toBe(5);
+    });
 
+    test("q.forEach", () => {
         let i = 9;
         let j = 0;
         q.forEach((item, k) => {
             expect(item).toBe(i--);
             expect(k).toBe(j++);
         });
+    });
 
-        expect(q.tail()).toBe(9);
-        expect(q.fromTail(0)).toBe(9);
-        expect(q.fromTail(3)).toBe(6);
+    test.each([
+        [0, 9],
+        [1, 8],
+        [2, 7],
+        [3, 6],
+        [4, 5],
+    ])("from tail(%i) is %i", (from, is) => {
+        expect(q.fromTail(from)).toBe(is);
+    });
+
+    test("fromTail(<greater than or equal to max size>) is undefined", () => {
         expect(q.fromTail(5)).toBe(undefined);
+    });
 
-        q.clear();
-        expect(q.size).toBe(0);
-        expect(q.tail()).toBe(undefined);
-        expect(q.fromTail(0)).toBe(undefined);
+    describe("q.clear()", () => {
+        beforeEach(() => q.clear());
+
+        test("q.size after q.clear() is 0", () => {
+            expect(q.size).toBe(0);
+        });
+        test("q.tail after q.clear() is undefined", () => {
+            expect(q.tail()).toBe(undefined);
+        });
+        test("q.fromTail(0) after q.clear() is undefined", () => {
+            expect(q.fromTail(0)).toBe(undefined);
+        });
     });
 });
