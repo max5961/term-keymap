9f1e260e8cf47a5df94cfb15add16caa5199e2c1
Maxwell Morrissey <mmorrissey92@proton.me>
2025-06-03 15:37:32 -0400

    Empty km always matches false.  Cleanup match test suites.

--- src/stateful/match.ts
+++ src/stateful/match.ts
@@ -13,14 +13,21 @@ export function match<T extends Pick<Data, "key" | "input">>(
         if (keymap.input.length > 1) return false;
         if (keymap.input.length !== data.input.size) return false;
         if (!data.input.has(keymap.input)) return false;
+    } else if (data.input.size) {
+        return false;
     }
-    if (keymap.key) {
+
+    if (keymap.key && keymap.key.length) {
         if (Array.isArray(keymap.key)) {
             if (!data.key.only(...keymap.key)) return false;
         } else {
             if (!data.key.only(keymap.key)) return false;
         }
+    } else if (data.key.size) {
+        return false;
     }
+
+    if ((!keymap.key || !keymap.key.length) && !keymap.input) return false;
 
     return true;
 }
--- test/match.test.ts
+++ test/match.test.ts
@@ -1,56 +1,93 @@
 import { describe, expect, test } from "vitest";
 import { match } from "../src/stateful/match.js";
 import { PeekSet } from "../src/util/PeekSet.js";
+import { KeyMap } from "../src/stateful/match.js";
+import { Data } from "../src/types.js";
 
 describe("match", () => {
-    test("input: 'a'", () => {
-        expect(
-            match(
-                { input: "a" },
-                { input: new PeekSet("a"), key: new PeekSet() },
-            ),
-        ).toBe(true);
+    // prettier-ignore
+    test.each<[boolean, KeyMap, Pick<Data, "key" | "input">]>([
+        [
+            true,
+            { input: "a" },
+            {input: new PeekSet("a"), key: new PeekSet() },
+        ],
+
+        [
+            false,
+            {},
+            {input: new PeekSet(["a"]), key: new PeekSet()},
+        ],
+
+        [
+            false,
+            {},
+            {input: new PeekSet(), key: new PeekSet(["ctrl"])},
+        ],
+
+        [
+            false,
+            {},
+            {input: new PeekSet("a"), key: new PeekSet(["ctrl"])},
+        ],
+        
+        [
+            false,
+            { input: "" },
+            { input: new PeekSet("a"), key: new PeekSet()},
+
+        ],
+
+        [
+            false,
+            { key: [] },
+            { input: new PeekSet(), key: new PeekSet(["ctrl"])},
+
+        ],
+
+        [
+            false,
+            { input: "ab" },
+            { input: new PeekSet("a"), key: new PeekSet() },
+        ],
+
+        [
+            true,
+            { input: "a", key: "ctrl" },
+            { input: new PeekSet("a"), key: new PeekSet(["ctrl"]) },
+        ],
+
+        [
+            false,
+            { input: "ab", key: "ctrl" },
+            { input: new PeekSet("a"), key: new PeekSet(["ctrl"]) },
+        ],
+
+        [
+            true,
+            { input: "a", key: ["ctrl", "super"] },
+            {
+                input: new PeekSet("a"),
+                key: new PeekSet(["ctrl", "super"]),
+            },
+        ],
+
+        [
+            false,
+            { input: "a", key: ["ctrl"] },
+            {
+                input: new PeekSet("a"),
+                key: new PeekSet(["ctrl", "super"]),
+            }
+        ],
+
+    ])("%o %o", (result, keymap, data) => {
+        expect(match(keymap, data)).toBe(result);
     });
 
-    test("input: 'ab'", () => {
-        expect(
-            match(
-                { input: "ab" },
-                { input: new PeekSet("a"), key: new PeekSet() },
-            ),
-        ).toBe(false);
-    });
-
-    test("input: 'a', key: 'ctrl'", () => {
-        expect(
-            match(
-                { input: "a", key: "ctrl" },
-                { input: new PeekSet("a"), key: new PeekSet(["ctrl"]) },
-            ),
-        ).toBe(true);
-    });
-
-    test("input: 'a', key: ['ctrl', 'super']", () => {
-        expect(
-            match(
-                { input: "a", key: ["ctrl", "super"] },
-                {
-                    input: new PeekSet("a"),
-                    key: new PeekSet(["ctrl", "super"]),
-                },
-            ),
-        ).toBe(true);
-    });
-
-    test("input: 'a', key: ['ctrl']", () => {
-        expect(
-            match(
-                { input: "a", key: ["ctrl"] },
-                {
-                    input: new PeekSet("a"),
-                    key: new PeekSet(["ctrl", "super"]),
-                },
-            ),
-        ).toBe(false);
+    test("{} can never match any Data", () => {
+        expect(match({}, { input: new PeekSet(), key: new PeekSet() })).toBe(
+            false,
+        );
     });
 });
