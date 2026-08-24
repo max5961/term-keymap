9d6e7d5ea01e5909a38fc470434cfc586ee3edb2
Maxwell Morrissey <mmorrissey92@proton.me>
2026-08-24 15:49:23 -0400

    fix: make possible for external libs to export key or KeyMapBuilderStarter

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,5 @@ export { setMouse } from "./terminal/setMouse.js";
 export { parseBuffer } from "./parsers/parseBuffer.js";
 export { KeyMapState } from "./stateful/KeyMapState.js";
 export { MouseState } from "./stateful/MouseState.js";
+export { KeyMapBuilderStarter } from "./util/KeyMapBuilder.js";
 export { key } from "./util/KeyMapBuilder.js";
